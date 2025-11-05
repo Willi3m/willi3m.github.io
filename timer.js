c0bb85d84e9b4f9819cae1bc69ee2f692aa04457
Willi3m <wphamle@horizon.csueastbay.edu>
2025-11-04 17:23:55 -0800

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,5 +1,21 @@
-
-let hour = document.getElementById("hour");
-let minutes= document.getElementById("minutes");
-let seconds = document.getElementById("seconds");
-console.log(hour.value)
+let totalSeconds = 10; 
+let totalMinutes = 0;
+let totalHours = 1;
+const myInterval = setInterval(() => {
+  if (totalSeconds < 0) {
+      totalSeconds = 59;
+      totalMinutes--;
+  }
+  if(totalMinutes < 0){
+    totalMinutes = 59;
+    totalHours--;
+  }
+  if(totalHours < 0){
+    clearInterval(myInterval);
+    document.getElementById("count").innerText = "done";
+    return;
+  }
+  document.getElementById("count").innerText = totalHours + "h" 
+  + totalMinutes + "m" + totalSeconds + "s"; 
+  totalSeconds--; 
+}, 1000);
